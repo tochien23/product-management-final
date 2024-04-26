d903c61b972eaecce606a27be48811eb8f738b6b
tochien23 <tovanchien@dtu.edu.vn>
2024-04-26 13:57:49 +0700

    update cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const multer = require('multer');
+
 const router = express.Router();
-const storageMulter = require("../../helpers/storageMulter")
-const upload = multer({ storage: storageMulter() });
+// const storageMulter = require("../../helpers/storageMulter")
+const upload = multer();
+// { storage: storageMulter() } 
+
 
 const controller = require("../../controllers/admin/product.controller");
 const validate = require("../../validates/admin/product.validate");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 router.get("/", controller.index);
 
@@ -20,6 +25,7 @@ router.get("/create", controller.create);
 router.post(
     "/create",
     upload.single('thumbnail'),
+    uploadCloud.upload,
     validate.createPost,
     controller.createPost
 );
