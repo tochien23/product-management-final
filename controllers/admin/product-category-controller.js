dce8c73cf634e97614ee3f1bb85bdb70ab714548
tochien23 <tovanchien@dtu.edu.vn>
2024-06-11 16:53:44 +0700

    Them danh muc san pham vao va tao them models ROLE

--- controllers/admin/product-category-controller.js
+++ controllers/admin/product-category-controller.js
@@ -1,4 +1,4 @@
-const ProductCategory = require("../../models/product-category.model")
+const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system")
 
@@ -20,6 +20,73 @@ module.exports.index = async (req, res) => {
         records: newRecords
     });
 };
+
+// [PATCH] / admin / products / change-status/ :status/ :id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status;
+    const id = req.params.id;
+
+    await ProductCategory.updateOne({ _id: id }, { status: status });
+
+    req.flash("success", "Cập nhật trạng thái thành công!");
+
+    res.redirect("back");
+}
+
+// [PATCH] / admin / products / change-multi
+module.exports.changeMulti = async (req, res) => {
+    const type = req.body.type;
+    const ids = req.body.ids.split(", ");
+
+    switch (type) {
+        case "active":
+            await ProductCategory.updateMany({ _id: { $in: ids } }, { status: "active" });
+            req.flash("success", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+            break;
+        case "inactive":
+            await ProductCategory.updateMany({ _id: { $in: ids } }, { status: "inactive" });
+            req.flash("success", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+            break;
+        case "delete-all":
+            await ProductCategory.updateMany(
+                { _id: { $in: ids } },
+                {
+                    deleted: true,
+                    deletedAt: new Date()
+                }
+            );
+            req.flash("success", `Đã xóa thành công ${ids.length} sản phẩm!`);
+            break;
+        case "change-position":
+            for (const item of ids) {
+                let [id, position] = item.split("-");
+                position = parseInt(position);
+                await ProductCategory.updateOne({ _id: id }, {
+                    position: position
+                });
+                req.flash("success", `Đã đổi vị trí thành công ${ids.length} sản phẩm!`);
+            }
+            break;
+        default:
+            break;
+    }
+
+    res.redirect("back");
+}
+
+// [DELETE] / admin / products-category / delete /:id
+module.exports.deleteItem = async (req, res) => {
+    const id = req.params.id;
+    // Xóa vĩnh viễn
+    // await Product.deleteOne({ _id: id });
+    // Xóa mềm 
+    await ProductCategory.updateOne({ _id: id }, {
+        deleted: true,
+        deletedAt: new Date()
+    });
+    req.flash("success", `Đã xóa thành công sản phẩm!`);
+    res.redirect("back");
+}
 
 // [GET] / admin / products-category / create
 module.exports.create = async (req, res) => {
@@ -52,3 +119,59 @@ module.exports.createPost = async (req, res) => {
 
     res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 }
+
+// [GET] / admin / products-category / edit /:id
+module.exports.edit = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const data = await ProductCategory.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        const records = await ProductCategory.find({
+            deleted: false
+        })
+
+        const newRecords = createTreeHelper.tree(records);
+
+        res.render("admin/pages/products-category/edit", {
+            pageTitle: "Chỉnh sửa danh mục sản phẩm",
+            data: data,
+            records: newRecords
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+    }
+};
+
+// [PATCH] / admin / products-category / edit /:id
+module.exports.editPatch = async (req, res) => {
+    const id = req.params.id;
+
+    req.body.position = parseInt(req.body.position);
+
+    await ProductCategory.updateOne({ _id: id, }, req.body);
+        
+    res.redirect("back");
+};
+
+// [GET] / admin / products-category / detail /:id
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            _id: req.params.id
+        };
+
+        const product = await ProductCategory.findOne(find);
+
+        res.render("admin/pages/products-category/detail", {
+            pageTitle: product.title,
+            product: product
+        });
+    } catch (error) {
+        res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+    }
+};
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -11,14 +11,33 @@ const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 
+router.patch("/change-status/:status/:id", controller.changeStatus);
+
+router.patch("/change-multi", controller.changeMulti);
+
+router.delete("/delete/:id", controller.deleteItem);
+
 router.get("/create", controller.create);
 
 router.post(
     "/create",
     upload.single('thumbnail'),
     uploadCloud.upload,
-    // validate.createPost,
+    validate.createPost,
     controller.createPost
 );
+
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+    "/edit/:id",
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.editPatch
+);
+
+router.get("/detail/:id", controller.detail);
+
 
 module.exports = router;
